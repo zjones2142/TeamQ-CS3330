20e66683b9f80a82d9a46986ecec495df60f3ec7
zjones2142 <zachjones2142@gmail.com>
2024-02-06 13:42:08 -0600

    zj - moved main method from Student to new Main class

---
+++ Homework_2/src/edu/mu/Main.java
@@ -0,0 +1,11 @@
+package edu.mu;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student student1 = new Student(0, "John", 3.0);
+		System.out.println(student1.toString());
+	}
+
+}
--- Homework_2/src/edu/mu/Student.java
+++ Homework_2/src/edu/mu/Student.java
@@ -14,11 +14,4 @@ public class Student {
 	public String toString() {
 		return "Name: " + name + "\nID: " + Integer.toString(id) + "\nGrade: " + Double.toString(grade) + "\n";
 	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Student student1 = new Student(0, "John", 3.0);
-		System.out.println(student1.toString());
-	}
-
 }
