38e2040397e5779839c62648834289451f8f6053
zjones2142 <zachjones2142@gmail.com>
2024-02-09 10:34:06 -0600

    zj - fix HW1 items for re-submission

--- Homework_1/src/edu/mu/Homework_1Class.java
+++ Homework_1/src/edu/mu/Homework_1Class.java
@@ -3,7 +3,16 @@ package edu.mu;
 public class Homework_1Class {
 
 	public static void main(String[] args) {
-			System.out.println("zjones2142\nbrxtz\nJosephMMun\nmdjvyz");
+			String array[][] = {
+				{"Zach Jones", "zjones2142"},
+				{"Rafa Bratfich de Oliveira", "nbrxtz"},
+				{"Joseph Mun", "JosephMMun"},
+				{"Matt Johnson", "nmdjvyz"}
+			};
+			for(int i=0;i<array.length;i++) {
+				System.out.println("Member "+i+":");
+				System.out.println(" Name: "+array[i][0]+"\n Git: "+array[i][1]+"\n");
+			}
 	}
 
 }
