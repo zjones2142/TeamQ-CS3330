6ada27305886afbd47ac37e92f7d62d638a789de
zjones2142 <zachjones2142@gmail.com>
2024-02-12 14:15:27 -0600

    zj - hw3 initial commit

---
+++ Homework_3/src/edu/mu/MediaProduct.java
@@ -0,0 +1,17 @@
+package edu.mu;
+
+public class MediaProduct {
+	String title;
+	double price;
+	int year;
+	enum genre {
+		ROCK,
+		POP,
+		JAZZ,
+		CLASSICAL,
+		HIP_HOP,
+		ELECTRONIC,
+		CHILDREN
+	}
+	
+}
---
+++ Homework_3/src/module-info.java
@@ -0,0 +1,2 @@
+module Homework_3 {
+}
